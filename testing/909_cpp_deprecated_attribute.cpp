44d398ba5aceae281849636dd311d917b023c72b
vst <volker@volker-steinlein.de>
2025-01-15 22:19:26 +0100

    Adjusted the 'deprecated test case files

--- testing/909_cpp_deprecated_attribute.cpp
+++ testing/909_cpp_deprecated_attribute.cpp
@@ -1,7 +1,20 @@
 // objective: test the C++ attribute 'deprecated'
+// 
+// The examples here are taken from
+// - https://en.cppreference.com/w/cpp/language/attributes/deprecated (since C++14)
+// - https://en.cppreference.com/w/c/language/attributes/deprecated   (since C23)
+//
 // check: 909__cpp__deprecated__attribute_8cpp.xml
 // check: class_deprecated01.xml
+// check: class_template_class.xml
+// check: class_template_class_3_01int_01_4.xml
+// check: index.xml
 // check: namespace_deprecated_namespace.xml
+
+    
+/// @todo A deprecated using definition named '`FLOAT_PTR`'
+// 
+using FLOAT_PTR [[deprecated("Do **NOT** use '`FLOAT_PTR`'")]] = float *;
 
 [[deprecated]]
 unsigned long deprecatedGlobalFunction00()
@@ -26,7 +39,8 @@ unsigned long deprecatedGlobalFunction02()
 enum [[deprecated("Do **NOT** use this enum any more) ]] DeprecatedEnum
 {
   OLD,
-  VERY_OLD
+  VERY_OLD  [[deprecated("'`VERY_OLD`' is deprecated"]] = 42,
+  DONT_KNOW [[deprecated("'`VERY_OLD`' is deprecated"]]
 };
 */
 
@@ -36,7 +50,8 @@ enum [[deprecated("Do **NOT** use this enum any more) ]] DeprecatedEnum
 class [[deprecated("The class '`Deprecated01`' is deprecated")]] Deprecated01
 {
   private:
-    [[deprecated("Do **NOT** use the typedef '`INT_PTR`' any more")]] typedef int* INT_PTR;
+    /// A deprecated typedef named '`INT_PTR`'
+    [[deprecated("Do **N O T** use the typedef '`INT_PTR`' any more")]] typedef int* INT_PTR;   
 
     /// deprecated attribute
     [[deprecated("'`deprecatedIntValue`' must **NOT** be used!")]] int deprecatedIntValue { 42 };
@@ -44,6 +59,16 @@ class [[deprecated("The class '`Deprecated01`' is deprecated")]] Deprecated01
     /** Does deprecated things.
      */
     void [[deprecated("Do **NOT** use '`deprecated()`' any more")]] deprecated();
+};
+
+template<typename TN>
+class TemplateClass
+{
+};
+
+/// deprecated template specialization
+template<> class [[deprecated("Do **NOT** use '`TemplateClass<int>`'")]] TemplateClass<int> 
+{
 };
 
 
